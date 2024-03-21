579d7503f4cc5f0431b4eea846da3a33986d63f5
Ян <peroff007@gmail.com>
2024-03-22 02:03:50 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задание 1");
+        System.out.println("Задание 1:");
         int i = 1;
         for (; i <= 10; i++) {
             System.out.println("i = " + i);
         }
         System.out.println(" ");
-        System.out.println("Задание 2");
+        System.out.println("Задание 2:");
         i = 10;
         for (;i >= 1; i-- ){
+            System.out.println("i = " + i);
+        }
+        System.out.println(" ");
+        System.out.println("Задание 3:");
+        i = 0;
+        for (; i < 17; i+=2){
             System.out.println("i = " + i);
         }
         System.out.println(" ");
