29713222aac8be4de914825c58fd285b6c8fd470
Ян <peroff007@gmail.com>
2024-03-22 02:11:52 +0300

    Задание 6

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ public class Main {
         System.out.println(" ");
         System.out.println("Задание 2:");
         i = 10;
-        for (;i >= 1; i-- ){
+        for (;i >= 1; i--){
             System.out.println("i = " + i);
         }
         System.out.println(" ");
         System.out.println("Задание 3:");
         i = 0;
-        for (; i < 17; i+=2){
+        for (; i < 17; i += 2){
             System.out.println("i = " + i);
         }
         System.out.println(" ");
@@ -28,6 +28,12 @@ public class Main {
         int year = 1904;
         for (;year <= 2096; year += 4){
             System.out.println( year + " год является високосным.");
+        }
+        System.out.println(" ");
+        System.out.println("Задание 6:");
+        i = 7;
+        for (;i <= 98; i += 7){
+            System.out.println("i = " + i);
         }
     }
 }
